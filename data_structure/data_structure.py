3fcf798c88d413cfa31c6f565afd4eec3d261b03
imwsl <wesley.yang789@hotmail.com>
2024-03-20 18:40:08 +0800

    data structure

---
+++ data_structure/data_structure.py
@@ -0,0 +1,16 @@
+# data_structure.py
+#
+
+# array
+# linked list
+# stack
+# queue
+# tree
+# graph
+# heap
+# hash table
+#
+
+# linear data structure: array, linked list, stack, queue, hash table
+# net data structure: graph
+#
